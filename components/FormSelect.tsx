b71642c7ecc78bbb7ad7c236a3eaeb76a921ca8c
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2025-04-26 12:28:48 -0400

    Add reusable form select component

--- components/FormSelect.tsx
+++ components/FormSelect.tsx
@@ -1,24 +1,35 @@
 import { InputHTMLAttributes } from "react";
 import clsx from "clsx";
 
-type FormLabelProps = InputHTMLAttributes<HTMLInputElement> & {
+export type Option = {
+  label: string;
+  value: string;
+}
+
+type FormSelectProps = InputHTMLAttributes<HTMLSelectElement> & {
+  data: Option[];
   label: string;
 };
 
-export default function FormLabel({
+export default function FormSelect({
   label,
+  data,
   className,
   ...props
-}: FormLabelProps) {
+}: FormSelectProps) {
   return (
     <label className={clsx("flex flex-col", className)}>
       <span className="font-semibold text-[14px] text-(--fly-text-primary)">
         {label}
       </span>
-      <input
+      <select
         className="bg-(--fly-white) placeholder-[--fly-input-placeholder-color] border-[1.5px] border-solid border-(--fly-border-color) px-[16px] h-[48px] rounded-[12px]"
         {...props}
-      />
+      >
+        {data.map((option) => {
+          return <option key={option.value} value={option.value}>{option.label}</option>
+        })}
+      </select>
     </label>
   );
 }
