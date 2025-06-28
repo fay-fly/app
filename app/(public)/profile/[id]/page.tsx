462b922e10a1dc13a005aeb071fbd005cf664969
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2025-06-28 17:48:10 -0400

    Fix deploy error

--- app/(public)/profile/[id]/page.tsx
+++ app/(public)/profile/[id]/page.tsx
@@ -1,7 +1,9 @@
-type Props = {
-  params: { id: string };
-};
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-export default function Profile({ params }: Props) {
+export default function ProfilePage({ params }: PageProps) {
   return <h1>Profile ID: {params.id}</h1>;
 }
