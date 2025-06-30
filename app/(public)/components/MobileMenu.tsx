a42a24f97c377da21d839e150e46e6a320f232b8
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2025-06-30 15:05:50 -0400

    Fix menu generation error

--- app/(public)/components/MobileMenu.tsx
+++ app/(public)/components/MobileMenu.tsx
@@ -7,7 +7,7 @@ export default function MobileMenu() {
   const { data: session, status } = useSession();
   return status !== "loading" && <div
     className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center h-[48px] z-50 md:hidden w-full">
-    {menuConfig.sort((a, b) => a.mobileIndex - b.mobileIndex).map(item => {
+    {[...menuConfig].sort((a, b) => a.mobileIndex - b.mobileIndex).map(item => {
       const MenuIcon = item.icon;
       return (!item.requiresAuth || (item.requiresAuth && session) )&& <MenuLink key={item.route} href={item.route}>
         <MenuIcon/>
