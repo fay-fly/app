73f847d8390ebabcc3a4fbe51c75fb697b4cf0c4
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2025-07-05 15:15:20 -0400

    Add hover effect on posts images

--- app/(public)/discover/page.tsx
+++ app/(public)/discover/page.tsx
@@ -2,6 +2,7 @@
 import type { PostWithUser } from "@/app/types/postWithUser";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import clsx from "clsx";
 
 export default function Discover() {
   const [posts, setPosts] = useState<PostWithUser[]>();
@@ -15,7 +16,7 @@ export default function Discover() {
   return (
     <div className="w-full bg-white mb-[48px] h-auto min-h-full">
       <div className="w-full mr-auto ml-auto max-w-[1000px]">
-        <h1 className="text-[#343434] text-[20px] font-semibold py-[16px]">
+        <h1 className="text-[#343434] text-[20px] font-semibold py-[16px] mx-[20px]">
           Discover
         </h1>
         <div className="grid grid-cols-3 gap-[2px]">
@@ -25,16 +26,20 @@ export default function Discover() {
                 return (
                   <div
                     key={post.id}
-                    className="w-full aspect-square overflow-hidden rounded bg-gray-100"
+                    className="w-full aspect-square overflow-hidden bg-gray-100 relative w-full h-full"
                   >
                     <img
                       src={post.imageUrl}
                       alt="publication"
-                      className="w-full h-full object-cover rounded"
+                      className="w-full h-full object-cover"
                     />
+                    <div className={clsx(
+                      "absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 opacity-0",
+                      "hover:opacity-70 transition-opacity duration-100 cursor-pointer",
+                    )}></div>
                   </div>
                 );
-              })}
+            })}
         </div>
       </div>
     </div>
