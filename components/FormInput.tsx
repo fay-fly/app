799903ae7490cf9aab12a13ed10096e43a7d0e30
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2026-01-11 10:33:48 -0500

    Add password eye

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,5 +1,7 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useState } from "react";
 import clsx from "clsx";
+import EyeOpen from "@/icons/EyeOpen";
+import EyeClosed from "@/icons/EyeClosed";
 
 type FormLabelProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
@@ -8,8 +10,12 @@ type FormLabelProps = InputHTMLAttributes<HTMLInputElement> & {
 export default function FormInput({
   label,
   className,
+  type,
   ...props
 }: FormLabelProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+
   return (
     <label className={clsx("flex flex-col", className)}>
       {label && (
@@ -17,10 +23,27 @@ export default function FormInput({
           {label}
         </span>
       )}
-      <input
-        className="bg-(--fly-white) placeholder-[--fly-input-placeholder-color] border-[1.5px] border-solid border-(--fly-border-color) px-[16px] h-[48px] rounded-[12px]"
-        {...props}
-      />
+      <div className="relative">
+        <input
+          type={isPassword && showPassword ? "text" : type}
+          className={clsx(
+            "w-full bg-(--fly-white) placeholder-[--fly-input-placeholder-color] border-[1.5px] border-solid border-(--fly-border-color) px-[16px] h-[48px] rounded-[12px]",
+            isPassword && "pr-[48px]"
+          )}
+          {...props}
+        />
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-[12px] top-1/2 -translate-y-1/2 p-[4px] text-[#a0a0a0] hover:text-[#5b5b5b] transition-colors cursor-pointer"
+            tabIndex={-1}
+            aria-label={showPassword ? "Hide password" : "Show password"}
+          >
+            {showPassword ? <EyeClosed /> : <EyeOpen />}
+          </button>
+        )}
+      </div>
     </label>
   );
 }
